f9515a82f938a989151189df966318ccf710b699
barrbaraf <barrbarafigueroa021@gmail.com>
2025-02-19 23:49:01 -0300

    .

--- FRONTEND/proyectoFRONT/vite.config.js
+++ FRONTEND/proyectoFRONT/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Configura la carpeta de salida para producción
-  },
   server: {
-    port: 3000, // Puedes cambiar el puerto si lo necesitas
+    port: 3000
   },
+  build: {
+    outDir: 'dist'
+  }
 });
